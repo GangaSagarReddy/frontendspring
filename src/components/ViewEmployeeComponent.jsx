b46fcf83f38163400ac316f53c75fc6d6d1651e2
GangaSagarReddy <101339550+GangaSagarReddy@users.noreply.github.com>
2023-06-06 09:05:50 +0530

    Update ViewEmployeeComponent.jsx

--- src/components/ViewEmployeeComponent.jsx
+++ src/components/ViewEmployeeComponent.jsx
@@ -29,7 +29,7 @@ class ViewEmployeeComponent extends Component {
         return (
             <div  style={{ backgroundImage:`url('https://wallpaper-mania.com/wp-content/uploads/2018/09/High_resolution_wallpaper_background_ID_77700383971.jpg')` , height:'700px'}}>
                 <br></br>
-                <div className = "  justify-content-center align-items-center" style={{ margin: '12rem'}}>
+                <div className = "  justify-content-center align-items-center" style={{ margin: '8rem'}}>
                 <div className = "card col-md-6 onset-md-5" >
                     <h3 className = "text-center" style={{fontFamily:'cursive',color:'blue',fontSize:30}}> View Employee Details</h3>
                     <div className = "card-body">
@@ -44,6 +44,22 @@ class ViewEmployeeComponent extends Component {
                         <div className = "row">
                             <label style={{fontFamily:'cursive',color:'blue'}}> Employee Email ID: </label>
                             <div> { this.state.employee.emailId }</div>
+                        </div>
+                        <div className = "row">
+                            <label style={{fontFamily:'cursive',color:'blue'}}> Employee Department: </label>
+                            <div> { this.state.employee.department }</div>
+                        </div>
+                        <div className = "row">
+                            <label style={{fontFamily:'cursive',color:'blue'}}> Employee Salary: </label>
+                            <div> { this.state.employee.salary }</div>
+                        </div>
+                        <div className = "row">
+                            <label style={{fontFamily:'cursive',color:'blue'}}> Employee Gender: </label>
+                            <div> { this.state.employee.gender }</div>
+                        </div>
+                        <div className = "row">
+                            <label style={{fontFamily:'cursive',color:'blue'}}> Employee DateOfBirth: </label>
+                            <div> { this.state.employee.dob }</div>
                         </div>
                     </div>
                     </div>
