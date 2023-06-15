3312f2a73795df20fc6e8075b1d13a012a147e84
GangaSagarReddy <101339550+GangaSagarReddy@users.noreply.github.com>
2023-06-15 12:08:16 +0530

    Update ViewEmployeeComponent.jsx

--- src/components/ViewEmployeeComponent.jsx
+++ src/components/ViewEmployeeComponent.jsx
@@ -25,12 +25,30 @@ class ViewEmployeeComponent extends Component {
             this.setState({employee: res.data});
         })
     }
-
+    handleLogout= () =>{
+        const confirm= window.confirm("Are you sure ?");
+        if(confirm){
+                window.location.href="/";
+            }
+            else{
+                window.location.href="/employees"
+            }
+     }
+    handleHomePage=() =>{
+        window.location.href="/employees";
+     }
     render() {
         return (
             
             <div  style={{ backgroundImage:`url('https://wallpaper-mania.com/wp-content/uploads/2018/09/High_resolution_wallpaper_background_ID_77700383971.jpg')` , height:'800px'}}>
-                <br></br>
+                 <div className = "contair"><br/>
+                        <div className='btn-group btn-group-lg d-flex ' role="group" aria-label="....">
+                            <button type="button" style={{backgroundColor:'black'}}  className="btn btn-outline-light w-100" onClick={()=>this.handleHomePage()}>Home Page</button>
+                            <button type="button" style={{backgroundColor:'black'}} className="btn btn-outline-light w-100 " >Add New Employee</button>
+                            <button type="button"style={{backgroundColor:'white'}} className="btn btn-outline-light w-100 active" >View</button>
+                            <button type="button" style={{backgroundColor:'black'}} className="btn btn-outline-light w-100" onClick={()=>this.handleLogout()}>{"LOGOUT"}</button>
+                        </div>
+                        </div>
                 <div>
                 <Link to='/employees'> <button className="btn btn-danger" size="xl" style={{marginLeft: "10px",size:'xl'}}>{"<<Back"}</button></Link>
                 </div>
@@ -39,7 +57,7 @@ class ViewEmployeeComponent extends Component {
                     <h3 className = "text-center" style={{fontFamily:'cursive',color:'blue',fontSize:30}}> View Employee Details</h3>
                     <div className = "card-body">
                         <div className = "row">
-                            <label style={{fontFamily:'cursive',color:'blue'}}> Employee First Name: </label>
+                            <label style={{fontFamily:'cursive',color:'blue'}}> Employee First Name:  </label>
                             <div> { this.state.employee.firstName }</div>
                         </div>
                         <div className = "row">
